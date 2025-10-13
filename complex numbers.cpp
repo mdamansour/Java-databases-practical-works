ed811fc45adbba647b06d91ed3bb6b61669e977e
Mohammed Amansour <m@amansour.me>
2025-10-13 21:38:08 +0100

    Introduced show as a seperate function

--- complex numbers.cpp
+++ complex numbers.cpp
@@ -5,10 +5,28 @@ class Complex {
 public:
     double real;
     double imag;
+
+    void show() {
+        if (real == 0 && imag == 0) {
+            cout << "0";
+        }
+        else if (real == 0) {
+            cout << imag << "i";
+        }
+        else if (imag == 0) {
+            cout << real;
+        }
+        else if (imag > 0) {
+            cout << real << " + " << imag << "i";
+        }
+        else {
+            cout << real << " - " << -imag << "i";  // print minus and make imag positive
+        }
+    }
 };
 
 int main() {
-    Complex a, b;      // two complex numbers
+    Complex a, b;
 
     cout << "Enter first complex number (real imag): ";
     cin >> a.real >> a.imag;
@@ -16,9 +34,11 @@ int main() {
     cout << "Enter second complex number (real imag): ";
     cin >> b.real >> b.imag;
 
-    cout << "\nYou entered:\n";
-    cout << "A = " << a.real << " + " << a.imag << "i\n";
-    cout << "B = " << b.real << " + " << b.imag << "i\n";
+    cout << "\nA = ";
+    a.show();
+    cout << "\nB = ";
+    b.show();
+    cout << endl;
 
     return 0;
 }
